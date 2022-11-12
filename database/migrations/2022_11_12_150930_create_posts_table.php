89ef65d54a30d551c0d58ac98b4babf9d4cef7db
Seiichiro <skyblue1011@icloud.com>
2022-11-13 00:14:41 +0900

    Created migrations for users, posts and emotions

--- database/migrations/2022_11_12_150930_create_posts_table.php
+++ database/migrations/2022_11_12_150930_create_posts_table.php
@@ -13,16 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('avatar',50)->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('role_id')->default(2)->comment('1:admin 2:user');
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id');
+            $table->string('image',50);
+            $table->text('description');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -33,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
